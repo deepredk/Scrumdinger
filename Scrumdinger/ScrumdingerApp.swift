01cccda38fd766a6458fac405a86cc79dc958675
Deepred <deepred.kim@gmail.com>
2022-07-13 14:27:03 +0900

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by 김진홍 on 2022/07/13.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
